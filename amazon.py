183f5c7b727695c8c8c6d080a8f45b8b690aaee7
Andres <andressanchez956@gmail.com>
2023-06-13 19:08:41 +0200

    first commit

---
+++ amazon.py
@@ -0,0 +1,52 @@
+import os
+import boto3
+import time
+import urllib
+import json
+
+transcribe_client = boto3.client('transcribe')
+
+def transcribe_file(job_name, file_uri, transcribe_client):
+    transcribe_client.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': file_uri},
+        MediaFormat='wav',
+        LanguageCode='en-US'
+    )
+
+    max_tries = 60
+    while max_tries > 0:
+        max_tries -= 1
+        job = transcribe_client.get_transcription_job(TranscriptionJobName=job_name)
+        job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+        if job_status in ['COMPLETED', 'FAILED']:
+            if job_status == 'COMPLETED':
+                response = urllib.request.urlopen(job['TranscriptionJob']['Transcript']['TranscriptFileUri'])
+                data = json.loads(response.read())
+                text = data['results']['transcripts'][0]['transcript']
+                
+            break
+
+def get_file_names():
+    path ="/Users/andressanchez/Dropbox/Mac/Desktop/participant_files/snippets/s101/s101_3"
+    #we shall store all the file names in this list
+    filelist = []
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            #append the file name to the list
+            filelist.append(file)
+
+    return sorted(filelist)
+
+def main():
+    files = get_file_names()
+
+    for f in files:
+        file_uri = 's3://dva-asr-bias/s101_3/' + f
+        job_name = 'job-' + f
+        transcribe_file(job_name, file_uri, transcribe_client)
+
+
+if __name__ == '__main__':
+    main()
